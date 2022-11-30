1ce349ed60281573b6f79d0224de17a14eaca40d
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-11-30 18:09:03 +0000

    Add restriction on Users

--- controllers/users.js
+++ controllers/users.js
@@ -11,6 +11,21 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
+
+  if(!username || username.length < 3) {
+    return response.status(400).json({ error: 'username must be at least 3 characters long' })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -39,6 +39,32 @@ describe('when there is initially one user in db', () => {
     expect(usernames).toContain(newUser.username)
   })
 
+
+  describe('when there is initially one user in db', () => {
+    // ...
+  
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
+  
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen',
+      }
+  
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+  
+      expect(result.body.error).toContain('username must be unique')
+  
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toEqual(usersAtStart)
+    })
+  })
+
   afterAll(() => {
     mongoose.connection.close()
   })
